715d19d5d22859a729c578e8d4a19085bd3824cd
mogurbon <mogurbon@gmail.com>
2020-08-18 22:18:12 -0500

    font aweasomw installation and principal blade template

---
+++ resources/views/main.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPH-EmKWcQtwUGHlUSfL9O4PtToZ8qupE&libraries=places"></script>
+</head>
+
+<body>
+<div id="app">
+
+    <header>
+
+
+    </header>
+    <content>
+       {{-- <div class="container">
+            @yield('content')
+        </div>--}}
+        <example-component></example-component>
+    </content>
+
+    <footer></footer>
+</div>
+</body>
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+{{--AIzaSyBPH-EmKWcQtwUGHlUSfL9O4PtToZ8qupE--}}
+
+
+
+
+</html>
